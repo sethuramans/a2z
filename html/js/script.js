73f0c2fb6be4774955366562c3a32289d19a24a9
Sethuraman <sethuramancbe@gmail.com>
2025-03-02 14:09:55 +0530

    Add files via upload

---
+++ html/js/script.js
@@ -0,0 +1,12 @@
+const tabs = document.querySelectorAll(".tab-nav-container .tab");
+tabs.forEach((clickedTab) => {
+  clickedTab.addEventListener("click", () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    clickedTab.classList.add("active");
+    const clickedTabBGColor =
+      getComputedStyle(clickedTab).getPropertyValue("color");
+    document.body.style.background = clickedTabBGColor;
+  });
+});
